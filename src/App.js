bbccb7c40b32dc51af7abff840cdeb3c3b127dfe
Suraj370 <surajpanda2077@gmail.com>
2023-08-17 20:40:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Tabs from './components/Tabs';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <Tabs/>
+        </header>
+    </div>
+  );
+}
+
+export default App;
